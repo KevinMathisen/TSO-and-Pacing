3ce9c41213c33762d367e4027ada8dd0ed64f4ba
Kevin Nikolai Mathisen <kevin.nikolai.mathisen@gmail.com>
2026-03-12 23:05:03 +0100

    test: update parse p4sta ts script to also write run name, and parse tshark tcp options field correctly

--- scripts/analysis/parse-p4sta-timestamps.py
+++ scripts/analysis/parse-p4sta-timestamps.py
@@ -3,24 +3,23 @@ import sys
 
 
 def ts1_from_tcp_options(opts: str):
-    # tshark prints tcp options as 0a:05:01...
     try:
-        b = bytes(int(x, 16) for x in opts.split(":") if x)
+        b = bytes.fromhex(opts.strip())
     except Exception:
         return ""
 
     i = 0
     while i < len(b)-1:
         kind = b[i]
-        if kind == 0:  # EOL
+        if kind == 0: # eol
             break
-        if kind == 1:  # NOP
+        if kind == 1: # nop
             i += 1
             continue
 
         length = b[i + 1]
 
-        # P4STA signature: kind 0x0f, length 0x10,
+        # p4sta signature: kind 0x0f, length 0x10,
         #  timestamp1 (6B), unused (2B), timestamp2 (6B)
         if kind == 0x0F and length == 0x10:
             ts1 = int.from_bytes(b[i+2 : i+8], "big")
@@ -32,22 +31,30 @@ def ts1_from_tcp_options(opts: str):
 
 
 def main():
-    if len(sys.argv) != 3:
-        print(f"Usage: {sys.argv[0]} INPUT.csv OUTPUT.csv", file=sys.stderr)
+    if len(sys.argv) != 5:
+        print(f"Usage: {sys.argv[0]} INPUT.csv OUTPUT.csv RUN_NAME RUN_NUM", file=sys.stderr)
         exit(1)
 
-    in_path, out_path = sys.argv[1], sys.argv[2]
+    in_path = sys.argv[1]
+    out_path = sys.argv[2]
+    run_name = sys.argv[3]
+    run_num = int(sys.argv[4])
 
     with open(in_path, "r", newline="") as fin, open(out_path, "w", newline="") as fout:
         reader = csv.DictReader(fin)
         writer = csv.writer(fout)
-        writer.writerow(["tcp.stream", "tcp.len", "timestamp1"])
+
+        writer.writerow(["run_name", "run_num", "stream_id", "tcp_len", "p4_timestamp_ns"])
 
         for row in reader:
-            stream = row.get("tcp.stream", "")
-            tcplen = row.get("tcp.len", "")
-            ts1 = ts1_from_tcp_options(row.get("tcp.options", ""))
-            writer.writerow([stream, tcplen, ts1])
+            stream = row["tcp.stream"]
+            tcp_len = row["tcp.len"]
+            ts1 = ts1_from_tcp_options(row["tcp.options"])
+
+            # Make stream ids unique across runs
+            stream_id = run_num * 10 + int(stream)
+
+            writer.writerow([run_name, run_num, stream_id, tcp_len, ts1])
 
 
 if __name__ == "__main__":
