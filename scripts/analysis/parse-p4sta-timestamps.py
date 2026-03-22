5dbd5fed9f94e6bcfd1f5be5afdfacbe720f3647
Kevin Nikolai Mathisen <kevin.nikolai.mathisen@gmail.com>
2026-03-22 11:01:44 +0100

    test: update p4sta ts parsing to read from payload

--- scripts/analysis/parse-p4sta-timestamps.py
+++ scripts/analysis/parse-p4sta-timestamps.py
@@ -2,32 +2,22 @@ import csv
 import sys
 
 
-def ts1_from_tcp_options(opts: str):
+def ts_from_payload(payload: str):
+    payload = payload.strip()
+    if not payload or len(payload) < 32:
+        return "", ""
+
     try:
-        b = bytes.fromhex(opts.strip())
+        b = bytes.fromhex(payload[:32])
     except Exception:
-        return ""
+        return "", ""
 
-    i = 0
-    while i < len(b)-1:
-        kind = b[i]
-        if kind == 0: # eol
-            break
-        if kind == 1: # nop
-            i += 1
-            continue
-
-        length = b[i + 1]
-
-        # p4sta signature: kind 0x0f, length 0x10,
-        #  timestamp1 (6B), unused (2B), timestamp2 (6B)
-        if kind == 0x0F and length == 0x10:
-            ts1 = int.from_bytes(b[i+2 : i+8], "big")
-            return str(ts1)
-
-        i += length
-
-    return ""
+    if b[0] != 0x0F or b[1] != 0x10:
+        return "", ""
+    
+    ts1 = int.from_bytes(b[2:8], "big")
+    ts2 = int.from_bytes(b[10:16], "big")
+    return str(ts1), str(ts2)
 
 
 def main():
@@ -44,17 +34,21 @@ def main():
         reader = csv.DictReader(fin)
         writer = csv.writer(fout)
 
-        writer.writerow(["run_name", "run_num", "stream_id", "tcp_len", "p4_timestamp_ns"])
+        writer.writerow(["run_name", "run_num", "stream_id", "tcp_len", "p4_timestamp_ns", "p4_timestamp_prev_ns"])
 
         for row in reader:
             stream = row["tcp.stream"]
             tcp_len = row["tcp.len"]
-            ts1 = ts1_from_tcp_options(row["tcp.options"])
+            ts1, ts2 = ts_from_payload(row["data.data"])
+
+            # Only keep full packets with timestamps
+            if int(tcp_len) < 1448 or not ts1:
+                continue
 
             # Make stream ids unique across runs
             stream_id = run_num * 10 + int(stream)
 
-            writer.writerow([run_name, run_num, stream_id, tcp_len, ts1])
+            writer.writerow([run_name, run_num, stream_id, tcp_len, ts1, ts2])
 
 
 if __name__ == "__main__":
