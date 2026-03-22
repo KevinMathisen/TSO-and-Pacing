588501467f0015b173fd7ab2afe6f2876ca818c7
Kevin Nikolai Mathisen <kevin.nikolai.mathisen@gmail.com>
2026-03-22 11:23:28 +0100

    test: add cpu plot and make plots prettier

--- scripts/analysis/generate-plots.py
+++ scripts/analysis/generate-plots.py
@@ -11,27 +11,35 @@ import matplotlib.ticker as mticker
 
 
 MICRO_BIN_US = 40
-FIRST_FLOW_TIMESERIES_MS = 5
+FIRST_FLOW_TIMESERIES_MS = 10
 
 SETUPS = [
-    "direct-link",
-    "datacenter",
-    "datacenter-hc",
-    "internet",
+    "direct-link_fq",
+    "datacenter_fq",
+    "internet_fq",
+    "direct-link_fq_codel",
 ]
+# SETUPS = ["datacenter_fq"]
 
 SOLUTIONS = {
     "no-tso": "No TSO",
     "tso": "TSO",
-    "tso-pacing": "TSO + Our Solution",
+    "tso-pacing": "TSO Pacing",
 }
 
 COLORS = {
-    "no-tso": "#E69F00",
-    "tso": "#009E73",
+    "no-tso": "#009E73",
+    "tso": "#E69F00",
     "tso-pacing": "#0072B2",
 }
 
+plt.rcParams.update({
+    "axes.titlesize": 20,
+    "axes.labelsize": 16,
+    "legend.fontsize": 16,
+    "xtick.labelsize": 14,
+    "ytick.labelsize": 14,
+})
 
 def packet_times_s(df: pd.DataFrame) -> np.ndarray:
     return df["p4_timestamp_ns"].to_numpy(dtype=np.float64) / 1e9
@@ -132,7 +140,7 @@ def aggregate_inter_departure_us(df: pd.DataFrame) -> np.ndarray:
 
 
 def load_solution(base_dir: Path, setup: str, solution: str) -> dict:
-    sol_dir = base_dir / f"{setup}_fq_{solution}"
+    sol_dir = base_dir / f"{setup}_{solution}"
 
     packets = pd.read_csv(sol_dir / "packets.csv")
     metrics = pd.read_csv(sol_dir / "metrics.csv")
@@ -149,14 +157,16 @@ def load_solution(base_dir: Path, setup: str, solution: str) -> dict:
     valid_stream_ids = flow_packet_counts[flow_packet_counts >= 100].index
     packets = packets[packets["stream_id"].isin(valid_stream_ids)]
 
-    # remove packets with no payload
+    # remove packets with no payload (although also do this in parse-p4sta.py)
     packets = packets[packets["tcp_len"] > 0]
 
-    # sort all packets
+    # sort all packets (although also do this in parse-p4sta.py)
     packets = packets.sort_values("p4_timestamp_ns", kind="mergesort")
 
     metrics["run_num"] = pd.to_numeric(metrics["run_num"])
     metrics["throughput_bps"] = pd.to_numeric(metrics["throughput_bps"])
+    metrics["cpu_sender"] = pd.to_numeric(metrics["cpu_sender"])
+    metrics["cpu_receiver"] = pd.to_numeric(metrics["cpu_receiver"])
 
     rtts = np.array(rtts, dtype=np.float64)
 
@@ -177,9 +187,10 @@ def prepare_solution_data(solution_data: dict) -> dict:
     metrics = solution_data["metrics"]
 
     throughput_bps = metrics["throughput_bps"].dropna().to_numpy(dtype=np.float64)
+    cpu_sender = metrics["cpu_sender"].dropna().to_numpy(dtype=np.float64)
+    cpu_receiver = metrics["cpu_receiver"].dropna().to_numpy(dtype=np.float64)
 
     first_flow_timeseries = get_first_flow_timeseries(packets, MICRO_BIN_US)
-    first_flow_t_s = get_first_flow_times(packets)
 
     per_flow_packets_per_bin = per_flow_packets_per_bin_distribution(packets, MICRO_BIN_US)
     per_flow_idt_us = per_flow_inter_departure_us(packets)
@@ -188,8 +199,9 @@ def prepare_solution_data(solution_data: dict) -> dict:
     return {
         **solution_data,
         "throughput_bps": throughput_bps,
+        "cpu_sender": cpu_sender,
+        "cpu_receiver": cpu_receiver,
         "first_flow_timeseries": first_flow_timeseries,
-        "first_flow_t_s": first_flow_t_s,
         "packets_per_bin": per_flow_packets_per_bin,
         "per_flow_idt_us": per_flow_idt_us,
         "aggregate_idt_us": aggregate_idt_us,
@@ -223,7 +235,7 @@ def _save_close(fig, path: Path):
     plt.close(fig)
 
 
-def plot_throughput_boxplot(solutions: list[dict], out_path: Path):
+def plot_throughput_boxplot(solutions: list[dict], setup: str, out_path: Path):
     fig = plt.figure(figsize=(8, 6))
 
     data = []
@@ -236,39 +248,116 @@ def plot_throughput_boxplot(solutions: list[dict], out_path: Path):
 
     for patch, sol in zip(bp["boxes"], solutions):
         patch.set_facecolor(sol["color"])
-        patch.set_alpha(0.45)
+        patch.set_alpha(1)
+
+    for median in bp["medians"]:
+        median.set_color("black")
 
     plt.ylabel("Throughput (Gbps)")
-    plt.title("Throughput distribution")
+    # plt.title("Throughput distribution")
     plt.grid(True, axis="y", linestyle="--", alpha=0.5)
 
     _save_close(fig, out_path)
 
+def plot_cpu_boxplot(solutions: list[dict], setup: str, out_path: Path):
+    fig, (ax_sender, ax_receiver) = plt.subplots(1, 2, figsize=(11, 6), sharex=True)
 
-def plot_rtt_boxplot(solutions: list[dict], out_path: Path):
+    labels = [sol["label"] for sol in solutions]
+    positions = np.arange(1, len(solutions) + 1)
+
+    sender_data = [sol["cpu_sender"] for sol in solutions]
+    receiver_data = [sol["cpu_receiver"] for sol in solutions]
+
+    bp_sender = ax_sender.boxplot(
+        sender_data,
+        positions=positions,
+        widths=0.6,
+        patch_artist=True,
+        labels=labels,
+    )
+
+    bp_receiver = ax_receiver.boxplot(
+        receiver_data,
+        positions=positions,
+        widths=0.6,
+        patch_artist=True,
+        labels=labels,
+    )
+
+    # color + medians
+    for i, sol in enumerate(solutions):
+        bp_sender["boxes"][i].set_facecolor(sol["color"])
+        bp_sender["boxes"][i].set_alpha(1)
+        bp_receiver["boxes"][i].set_facecolor(sol["color"])
+        bp_receiver["boxes"][i].set_alpha(1)
+
+    for med in bp_sender["medians"]:
+        med.set_color("black")
+    for med in bp_receiver["medians"]:
+        med.set_color("black")
+
+    ax_sender.set_title("Sender CPU")
+    ax_receiver.set_title("Receiver CPU")
+
+    ax_sender.set_ylabel("Average CPU usage (%)")
+
+    ax_sender.grid(True, axis="y", linestyle="--", alpha=0.5)
+    ax_receiver.grid(True, axis="y", linestyle="--", alpha=0.5)
+
+    ax = plt.gca()
+    if setup in ["direct-link_fq", "direct-link_fq_codel", "datacenter_fq"]:
+        plt.ylim(bottom=0)
+        ax.xaxis.set_major_locator(mticker.MultipleLocator(0.5))
+    elif setup == "internet_fq":
+        plt.ylim(bottom=50)
+
+    # no legend needed
+    _save_close(fig, out_path)
+
+
+def plot_rtt_boxplot(solutions: list[dict], setup: str, out_path: Path):
     fig = plt.figure(figsize=(8, 6))
 
     data = []
     labels = []
     for sol in solutions:
-        data.append(sol["rtts"])
+        data.append(sol["rtts"] / 1000.0) # us to ms
         labels.append(sol["label"])
 
     bp = plt.boxplot(data, patch_artist=True, labels=labels, widths=0.6)
 
     for patch, sol in zip(bp["boxes"], solutions):
         patch.set_facecolor(sol["color"])
-        patch.set_alpha(0.45)
+        patch.set_alpha(1)
 
-    plt.ylabel("RTT")
-    plt.title("RTT distribution")
+    for median in bp["medians"]:
+        median.set_color("black")
+
+    ax = plt.gca()
+    if setup in ["direct-link_fq", "direct-link_fq_codel", "datacenter_fq"]:
+        plt.ylim(bottom=0)
+        ax.xaxis.set_major_locator(mticker.MultipleLocator(0.5))
+    elif setup == "internet_fq":
+        plt.ylim(bottom=50)
+
+
+    plt.ylabel("RTT (ms)")
+    # plt.title("RTT distribution")
     plt.grid(True, axis="y", linestyle="--", alpha=0.5)
 
     _save_close(fig, out_path)
 
 
-def plot_firstflow_timeseries(solutions: list[dict], out_path: Path):
+def plot_firstflow_timeseries(solutions: list[dict], setup: str, out_path: Path):
     fig = plt.figure(figsize=(10, 6))
+
+    x_start, x_end = 0, FIRST_FLOW_TIMESERIES_MS
+    if setup in ["direct-link_fq", "direct-link_fq_codel"]:
+        x_start, x_end = 100, 103
+    elif setup == "datacenter_fq":
+        x_start, x_end = 100, 105
+    elif setup == "internet_fq":
+        x_start, x_end = 100, 108
 
     for sol in solutions:
         df = sol["first_flow_timeseries"]
@@ -276,12 +365,16 @@ def plot_firstflow_timeseries(solutions: list[dict], out_path: Path):
         x_ms = (x - float(x[0])) * 1000.0
         y = df["bin_packets"].to_numpy()
 
-        mask = x_ms <= FIRST_FLOW_TIMESERIES_MS
+        mask = x_ms >= x_start and x_ms <= x_end
         x_ms = x_ms[mask]
         y = y[mask]
-
-        plt.plot(x_ms, y, label=sol["label"], color=sol["color"], linewidth=1.4)
-        plt.fill_between(x_ms, y, 0, color=sol["color"], alpha=0.22, linewidth=0)
+        
+        # place tso pacing behind
+        if sol["label"] == "TSO Pacing":
+            plt.plot(x_ms, y, label=sol["label"], color=sol["color"], linewidth=1.4, zorder=-1)
+            plt.fill_between(x_ms, y, 0, color=sol["color"], alpha=0.22, linewidth=0)
+        else:
+            plt.plot(x_ms, y, label=sol["label"], color=sol["color"], linewidth=1.4, zorder=1)
 
     ax = plt.gca()
     ax.xaxis.set_major_locator(mticker.MultipleLocator(1))
@@ -299,10 +392,12 @@ def plot_firstflow_timeseries(solutions: list[dict], out_path: Path):
     ax.tick_params(axis="x", which="major", length=6, width=1.0, labelsize=14)
     ax.tick_params(axis="y", which="both", right=True, labelright=False)
 
-    plt.xlim(0, FIRST_FLOW_TIMESERIES_MS)
+    plt.ylim(0, 35)
+    plt.xlim(x_start, x_end)
+
     plt.xlabel("Time elapsed (ms)")
     plt.ylabel(f"Packets per {MICRO_BIN_US} µs bin")
-    plt.title(f"Packet timeseries ({FIRST_FLOW_TIMESERIES_MS} ms)")
+    # plt.title(f"Packet timeseries ({FIRST_FLOW_TIMESERIES_MS} ms)")
     plt.legend(loc='upper right')
 
     _save_close(fig, out_path)
@@ -334,19 +429,19 @@ def plot_packets_per_bin_violin(solutions: list[dict], out_path: Path):
 
     plt.xticks(positions, [sol["label"] for sol in solutions])
     plt.ylabel(f"Packets per {MICRO_BIN_US} µs bin")
-    plt.title(f"Distribution of packets per {MICRO_BIN_US} µs bin")
+    # plt.title(f"Distribution of packets per {MICRO_BIN_US} µs bin")
     plt.grid(True, axis="y", linestyle="--", alpha=0.5)
 
     _save_close(fig, out_path)
 
 
-def plot_cdf(solutions: list[dict], value_key: str, xlabel: str, out_path: Path):
+def plot_cdf(solutions: list[dict], setup: str, value_key: str, xlabel: str, out_path: Path):
     fig = plt.figure(figsize=(10, 6))
 
     for s in solutions:
         values = s[value_key]
         x, y = cdf_xy(values)
-        plt.plot(x, y, label=s["label"], color=s["color"], linewidth=1.6)
+        plt.plot(x, y, label=s["label"], color=s["color"], linewidth=2)
 
     ax = plt.gca()
     ax.set_xscale("log")
@@ -357,8 +452,9 @@ def plot_cdf(solutions: list[dict], value_key: str, xlabel: str, out_path: Path)
     ax.tick_params(axis="x", which="both", top=True, labeltop=False)
     ax.tick_params(axis="y", which="both", right=True, labelright=False)
 
-    ax.grid(True, which="major", axis="x", alpha=0.7, linestyle="--", linewidth=0.5)
+    ax.grid(True, which="major", axis="x", alpha=0.7, linestyle="--", linewidth=0.7)
 
+    plt.xlim(1, 1000)
     plt.xlabel(xlabel)
     plt.ylabel("Cumulative Probability")
     plt.legend()
@@ -374,17 +470,22 @@ def write_setup_plots(setup_result: dict, plots_dir: Path):
     setup_dir.mkdir(parents=True, exist_ok=True)
 
     plot_throughput_boxplot(
-        solutions,
+        solutions, setup,
         setup_dir / "throughput_boxplot.png",
     )
 
     plot_rtt_boxplot(
-        solutions,
+        solutions, setup,
         setup_dir / "rtt_boxplot.png",
     )
 
+    plot_cpu_boxplot(
+        solutions, setup,
+        setup_dir / "cpu_boxplot.png"
+    )
+
     plot_firstflow_timeseries(
-        solutions,
+        solutions, setup,
         setup_dir / f"timeseries_{MICRO_BIN_US}us.png",
     )
 
@@ -394,13 +495,13 @@ def write_setup_plots(setup_result: dict, plots_dir: Path):
     )
 
     plot_cdf(
-        solutions, "per_flow_idt_us",
+        solutions, setup, "per_flow_idt_us",
         xlabel="Inter-departure time within flow (µs)",
         out_path=setup_dir / "per_flow_idt_cdf.png",
     )
 
     plot_cdf(
-        solutions, "aggregate_idt_us",
+        solutions, setup, "aggregate_idt_us",
         xlabel="Inter-departure time across all flows in run (µs)",
         out_path=setup_dir / "aggregate_idt_cdf.png",
     )
